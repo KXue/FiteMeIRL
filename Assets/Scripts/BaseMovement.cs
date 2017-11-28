9f29a4e74f3e359511481ce1f620a3184137898e
KevinXue <ivexnuke@gmail.com>
2017-11-28 18:26:30 -0500

    Added movement animations for a battlemage. Added basic movement controller

---
+++ Assets/Scripts/BaseMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovement : MonoBehaviour {
+	#region Public Variables
+	public float m_maxSpeed;
+	public float m_sprintSpeed;
+	private Vector3 m_moveDirection;
+	private Animator m_animationController;
+	private CharacterController m_characterController;
+	[SerializeField]
+	private bool m_jump = false;
+	#endregion
+	// Use this for initialization
+	void Start () {
+		m_animationController = GetComponent<Animator>();
+		m_characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_moveDirection = Vector3.zero;
+
+		m_moveDirection.z = Input.GetAxis("Vertical");
+		m_moveDirection.x = Input.GetAxis("Horizontal");
+		m_jump = Input.GetButtonDown("Jump");
+
+		// m_characterController.Move(m_moveDirection);
+		m_animationController.SetFloat("Right", m_moveDirection.x);
+		m_animationController.SetFloat("Forward", m_moveDirection.z);
+		m_animationController.SetFloat("Speed", m_moveDirection.magnitude);
+		if(m_jump){
+			m_animationController.SetBool("Jump", m_jump);
+			m_jump = false;
+		}
+	}
+}
