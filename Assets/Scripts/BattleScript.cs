ef0cf71463f3fae3d6f897c2a76c7e591d57f320
Maaz Haq <Maaz_haq123@hotmail.com>
2017-11-29 18:42:50 -0500

    added battlescript

---
+++ Assets/Scripts/BattleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleScript : MonoBehaviour {
+
+	private Animator m_animator;
+	private bool m_isAttacking = false;
+
+	void Start() {
+		m_animator = GetComponent<Animator>();
+	}
+
+	void Update() {
+		if(Input.GetButtonDown("Attack")) {
+			m_animator.SetTrigger("isAttacking");
+		}
+	}
+}
