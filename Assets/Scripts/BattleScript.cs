893a5327913456278a65cc06a8278de945616c21
Kevin Xue <ivexnuke@gmail.com>
2017-12-01 07:49:14 -0500

    Implemented crosshair tracking. Added HUD and Menu

--- Assets/Scripts/BattleScript.cs
+++ Assets/Scripts/BattleScript.cs
@@ -6,29 +6,47 @@ public class BattleScript : MonoBehaviour {
 
 	public Transform prefab;
 	public GameObject prefabSpawnPoint;
+	public float m_fireBallSpeed = 30.0f;
+	private bool m_isAttacking = false;
 
 	private Animator m_animator;
-	private bool m_isAttacking = false;
 
 	void Start() {
 		m_animator = GetComponent<Animator>();
 	}
 
 	void Update() {
-		if(Input.GetButtonDown("Attack") && !m_isAttacking) {
+		if(Input.GetButton("Fire2") && !m_isAttacking) {
 			m_animator.SetTrigger("isAttacking");
 			m_isAttacking = true;
 		}
 	}
 
 	public void SpawnAttack() {
-		var fireBall = Instantiate(prefab, prefabSpawnPoint.transform.position, transform.rotation);
-		fireBall.GetComponent<Rigidbody>().velocity = fireBall.transform.forward * 6;
+		var fireBall = Instantiate(prefab, prefabSpawnPoint.transform.position, AimAtCrosshair());
+		fireBall.GetComponent<Rigidbody>().velocity = fireBall.transform.forward * m_fireBallSpeed;
 
 		Destroy(fireBall.gameObject, 2.0f);
 	}
-
-	public void EndOfAttack() {
+	public void EndAttack(){
 		m_isAttacking = false;
+	}
+
+	Quaternion AimAtCrosshair(){
+		Camera thirdPersonCamera = Camera.main;
+        Quaternion retVal = thirdPersonCamera.transform.rotation;
+        RaycastHit hitInfo;
+		Ray aimRay = thirdPersonCamera.ScreenPointToRay(new Vector3(thirdPersonCamera.pixelWidth * 0.5f, thirdPersonCamera.pixelHeight * 0.5f, 0));
+
+		bool didHit = Physics.Raycast(
+			aimRay.origin, 
+			aimRay.direction, 
+			out hitInfo, 
+			Mathf.Infinity, 
+			LayerMask.NameToLayer("Ground") | LayerMask.NameToLayer("Enemy"));
+		if(didHit){
+			retVal = Quaternion.LookRotation(hitInfo.point - prefabSpawnPoint.transform.position);
+		}
+		return retVal;
 	}
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private static GameManager instance;
+    // Static singleton property
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance ?? (instance = new GameObject("Singleton").AddComponent<GameManager>());
+        }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        instance = this;
+		DontDestroyOnLoad(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
