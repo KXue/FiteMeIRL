21bb58a7e74d4e605e58a30eb5e8923659bc5561
Maaz Haq <Maaz_haq123@hotmail.com>
2017-11-30 19:40:31 -0500

    finished attack animation with a fireball

--- Assets/Scripts/BattleScript.cs
+++ Assets/Scripts/BattleScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BattleScript : MonoBehaviour {
 
+	public Transform prefab;
+	public GameObject prefabSpawnPoint;
+
 	private Animator m_animator;
 	private bool m_isAttacking = false;
 
@@ -12,8 +15,20 @@ public class BattleScript : MonoBehaviour {
 	}
 
 	void Update() {
-		if(Input.GetButtonDown("Attack")) {
+		if(Input.GetButtonDown("Attack") && !m_isAttacking) {
 			m_animator.SetTrigger("isAttacking");
+			m_isAttacking = true;
 		}
+	}
+
+	public void SpawnAttack() {
+		var fireBall = Instantiate(prefab, prefabSpawnPoint.transform.position, transform.rotation);
+		fireBall.GetComponent<Rigidbody>().velocity = fireBall.transform.forward * 6;
+
+		Destroy(fireBall.gameObject, 2.0f);
+	}
+
+	public void EndOfAttack() {
+		m_isAttacking = false;
 	}
 }
